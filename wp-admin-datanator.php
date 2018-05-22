21645b7122c0433a3bd976972c7108f4378eca70
Gilad Ehven <gilad@ehven.com>
2018-05-22 17:07:34 -0400

    First localhost commit: Base file system (including untested [composer.json]); parallels 'wordpress-plugin-starter-type-four'.

---
+++ wp-admin-datanator.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * WordPress plugin that adds controls and data (info about the current site/project) to various Admin screens
+ *
+ * @author                  Gilad Ehven
+ * @copyright               2018 Gilad Ehven (Ehventerprise LLC)
+ * @license                 GNU General Public License 2.0+
+ * @link                    https://github.com/GiladEhven/wp-admin-datanator
+ * @package                 WpAdminDatanator
+ * @since                   0.1.0
+ *
+ * @wordpress-plugin
+ *
+ * Plugin Name:             WP Admin Datanator
+ *
+ * Author:                  Gilad Ehven
+ * Author URI:              http://gilad-ehven.com
+ * Contributors:            Mihal Ehven (Ehventerprise LLC)
+ * Description:             This plugin adds controls and data (info about the current site/project) to various Admin screens.
+ * Domain Path:             /languages
+ * License:                 GNU General Public License 2.0+
+ * License URI:             http://www.gnu.org/licenses/gpl-2.0.txt
+ * Network:                 False
+ * Plugin URI:              https://github.com/GiladEhven/wp-admin-datanator
+ * Privacy Policy:          https://gilad-ehven.com/privacy/
+ * Terms of Use:            https://gilad-ehven.com/terms/
+ * Text Domain:             wp-admin-datanator
+ * Version:                 0.1.0
+ *
+ */
+
+namespace WpAdminDatanator;
+
+if ( ! defined( 'ABSPATH' ) ) exit( "Nothing to see here. Sorry. Try the home page." );
